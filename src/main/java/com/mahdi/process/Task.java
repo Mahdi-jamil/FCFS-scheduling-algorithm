27908d3fbd8d55d50bea34b865800df474051023
Mahdi-jamil <134617316+Mahdi-jamil@users.noreply.github.com>
2023-10-02 05:58:32 -0700

    Delete src/main/java/com/mahdi directory

--- src/main/java/com/mahdi/process/Task.java
+++
@@ -1,9 +0,0 @@
-package com.mahdi.process;
-
-public interface Task {
-	long calculateCT();
-	float calculateTT();
-	float calculateWT();
-	void calculateCpuBurstTime();
-	void generateProcessReport();
-}
